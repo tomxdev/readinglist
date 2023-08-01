afea3f867e5f713ca40c9beb9567251d28f0dd28
Wellington Alves das Neves <devtomx@gmail.com>
2023-08-01 20:31:04 -0300

    feat: update impl main

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,13 +4,11 @@ import (
 	"database/sql"
 	"flag"
 	"fmt"
+	_ "github.com/lib/pq"
 	"log"
 	"net/http"
 	"os"
 	"readinglist/internal/data"
-	"time"
-
-	_ "github.com/lib/pq"
 )
 
 const version = "1.0.0"
@@ -18,32 +16,28 @@ const version = "1.0.0"
 type config struct {
 	port int
 	env  string
-	dsn  string
+	db   struct {
+		dsn string
+	}
 }
 
 type application struct {
-	config config
+	config config //type embedding
 	logger *log.Logger
 	models data.Models
-}
-
-type book struct {
-	id    int
-	title string
-	year  int
 }
 
 func main() {
 	var cfg config
 
-	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
-
 	flag.IntVar(&cfg.port, "port", 4000, "API server port")
 	flag.StringVar(&cfg.env, "env", "dev", "Environment (dev|stage|prod")
-	flag.StringVar(&cfg.dsn, "db-dsn", os.Getenv("READINGLIST_DB_DSN"), "PostgreSQL DSN")
+	flag.StringVar(&cfg.db.dsn, "db-dsn", os.Getenv("READINGLIST_DB_DSN"), "PostgreSQL DSN")
 	flag.Parse()
 
-	db, err := sql.Open("postgres", cfg.dsn)
+	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+
+	db, err := sql.Open("postgres", cfg.db.dsn)
 	if err != nil {
 		logger.Fatal(err)
 	}
@@ -55,25 +49,23 @@ func main() {
 		logger.Fatal(err)
 	}
 
-	logger.Printf("database connection pool established.")
+	logger.Printf("database connection pool established")
 
 	app := &application{
 		config: cfg,
 		logger: logger,
 		models: data.NewModels(db),
 	}
-
 	addr := fmt.Sprintf(":%d", cfg.port)
 
-	srv := &http.Server{
-		Addr:         addr,
-		Handler:      app.route(),
-		IdleTimeout:  time.Minute,
-		ReadTimeout:  10 * time.Second,
-		WriteTimeout: 30 * time.Second,
-	}
+	http.HandleFunc("/", app.notFoundResponse) // route for custom 404 reponse
+	http.HandleFunc("/v1/healthcheck", app.healthcheckHandler)
+	http.HandleFunc("/v1/books", app.bookHandler)
+	http.HandleFunc("/v1/books/", app.multiplexer)
 
 	logger.Printf("starting %s server on %s", cfg.env, addr)
-	err = srv.ListenAndServe()
-	logger.Fatal(err)
+	err = http.ListenAndServe(addr, nil)
+	if err != nil {
+		logger.Fatal(err)
+	}
 }
